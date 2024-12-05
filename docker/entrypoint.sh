38f74c21b99ad8b459ded11085cb03e089ebee7a
inmaytide <inmaytide@gmail.com>
2024-12-05 10:27:39 +0800

    💄 Optimize Dependencies and Related Configurations.2

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@ java -Djava.security.egd=file:/dev/./urandom \
             ${JVM_OPTS} \
             -jar /home/app.jar \
             --spring.profiles.active=${PROFILE:-prod} \
-            --spring.cloud.nacos.config.server-addr=${NACOS_SERVER:-127.0.0.1:8848}
-            --spring.cloud.nacos.config.username=${NACOS_USERNAME:nacos}
+            --spring.cloud.nacos.config.server-addr=${NACOS_SERVER:-127.0.0.1:8848} \
+            --spring.cloud.nacos.config.username=${NACOS_USERNAME:nacos} \
             --spring.cloud.nacos.config.password=${NACOS_PASSWORD:nacos}
