f8a2b80373e1b81ed61de09561ecf6ab77d52cda
inmaytide <inmaytide@gmail.com>
2024-12-06 17:40:10 +0800

    💄 Optimize Dependencies and Related Configurations.3

--- src/main/java/com/inmaytide/orbit/core/configuration/ApplicationProperties.java
+++ src/main/java/com/inmaytide/orbit/core/configuration/ApplicationProperties.java
@@ -1,17 +1,8 @@
 package com.inmaytide.orbit.core.configuration;
 
 import com.inmaytide.orbit.commons.configuration.GlobalProperties;
-import com.inmaytide.orbit.commons.utils.CommonUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ReflectionUtils;
-
-import java.lang.reflect.Field;
-import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author inmaytide
