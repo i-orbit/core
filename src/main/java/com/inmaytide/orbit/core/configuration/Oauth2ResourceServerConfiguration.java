82fabb5117527a7674dee81f6fcc80cb9ba58cff
inmaytide <inmaytide@gmail.com>
2025-01-03 11:23:46 +0800

    refactor: extract security-related common configurations to simplify code

---
+++ src/main/java/com/inmaytide/orbit/core/configuration/Oauth2ResourceServerConfiguration.java
@@ -0,0 +1,37 @@
+package com.inmaytide.orbit.core.configuration;
+
+import com.inmaytide.exception.web.servlet.DefaultHandlerExceptionResolver;
+import com.inmaytide.orbit.commons.security.Oauth2ResourceServerConfigurationAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author inmaytide
+ * @since 2024/4/18
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@DependsOn("exceptionResolver")
+public class Oauth2ResourceServerConfiguration extends Oauth2ResourceServerConfigurationAdapter {
+
+    public Oauth2ResourceServerConfiguration(DefaultHandlerExceptionResolver exceptionResolver, ApplicationProperties properties) {
+        super(exceptionResolver, properties);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, DefaultHandlerExceptionResolver exceptionResolver, ApplicationProperties properties) throws Exception {
+        return super.configure(http).authorizeHttpRequests(c -> {
+            c.requestMatchers("/v3/api-docs").permitAll();
+            c.requestMatchers(HttpMethod.GET, "/api/system/properties").permitAll();
+            c.anyRequest().authenticated();
+        }).build();
+    }
+
+}
